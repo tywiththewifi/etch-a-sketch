72f032a50768dfd832134937a3758aef70fa7840
tywiththewifi <156716969+tywiththewifi@users.noreply.github.com>
2024-02-14 16:01:34 -0800

    Create etch-a-sketch type grid page using html, css, javascript

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const container = document.querySelector('#container');
+
+
+function createGrid(n) {
+
+    container.innerHTML = '';
+
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement('div');
+        row.className = "row";
+        
+        for (let j = 0; j < n; j++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.className = "gridSquare";
+            row.appendChild(gridSquare);
+            gridSquare.addEventListener('mouseover', () => {
+                gridSquare.style.backgroundColor = 'red';
+            });
+            
+        };
+
+        container.appendChild(row);
+    }
+}
+
+createGrid(16);
+
+
+const newGrid = document.querySelector('#button');
+
+newGrid.addEventListener('click', () => {
+    let userInput = prompt("How many squares per side for the new grid? Please choose a number less than 100");
+    if (userInput < 100) {
+        createGrid(userInput);
+    } else {
+        alert("Please enter a number smaller than 100")
+    }
+});
+
