1f9841a0f9f01870fbecc15e953d4239f2401f82
tywiththewifi <156716969+tywiththewifi@users.noreply.github.com>
2024-02-14 16:11:47 -0800

    Modify js code so that each square turns a random rgb color when mouseover executes

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ function createGrid(n) {
             gridSquare.className = "gridSquare";
             row.appendChild(gridSquare);
             gridSquare.addEventListener('mouseover', () => {
-                gridSquare.style.backgroundColor = 'red';
+
+                const red = Math.floor(Math.random() * 256);
+                const green = Math.floor(Math.random() * 256);
+                const blue = Math.floor(Math.random() * 256);
+
+                gridSquare.style.backgroundColor = `rgb(${red},${green},${blue})`;
             });
             
         };
